3077a49f399d287ac39fa0103dfffa8475c3b076
kim <52jinchao@163.com>
2023-03-03 21:49:05 +0800

    incomplete

--- java-basic/src/main/java/com/study/algorithm/ConsistentHashing.java
+++ java-basic/src/main/java/com/study/algorithm/ConsistentHashing.java
@@ -1,37 +1,57 @@
 package com.study.algorithm;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * TODO: incomplete
  */
 public class ConsistentHashing {
-
-
+    static List<Node> nodes = new ArrayList<>();
+    static List<VirtualNode> virtualNodeList = new ArrayList<>();
+    static int nodeSize = 3;
+    static int totalVirtualNodeSize = 30;
     public static void main(String[] args) {
-        List<VirtualNode> virtualNodeList = new ArrayList<>();
-        for(int i=0;i<3;i++){
-            for(int j=1;j<10;j++){
-                VirtualNode virtualNode = new VirtualNode("redis"+i,j);
+
+
+        for(int i=0;i<nodeSize;i++){
+            Node node = new Node("redis"+i);
+            for(int j=1;j<=totalVirtualNodeSize/nodeSize;j++){
+                VirtualNode virtualNode = new VirtualNode(node,j,totalVirtualNodeSize);
                 virtualNodeList.add(virtualNode);
             }
+            nodes.add(node);
         }
         System.out.println(virtualNodeList);
+        int predictCount = totalVirtualNodeSize/2;
 
-        System.out.println("sss".hashCode());
-        System.out.println("fffsssss".hashCode());
+        for(int i=0;i<predictCount;i++){
+            String uuid = UUID.randomUUID().toString();
+            int index = Math.abs(uuid.hashCode())%(totalVirtualNodeSize);
+            System.out.println("uuid:"+uuid+",index:"+index);
+        }
+
+    }
+
+    static VirtualNode findVirtualNode(Object item){
+        int index = Math.abs(item.hashCode())%(totalVirtualNodeSize);
+        for(int i=0;i<totalVirtualNodeSize;i++){
+
+        }
+        return null;
     }
 
     static class VirtualNode implements Comparable<VirtualNode>{
         public int hashCode;
-        public String realNodeName;
+        public Node node;
+
+        public int index;
 
         public String virtualNodeName;
 
-        public VirtualNode(String realNodeName,int i){
-            this.virtualNodeName = realNodeName+"#"+i;
+        public VirtualNode(Node node,int i,int totalVirtualNodeSize){
+            this.virtualNodeName = node.name+"#"+i;
             this.hashCode = Math.abs(this.virtualNodeName.hashCode());
+            index = Math.abs(hashCode)%totalVirtualNodeSize;
         }
 
         @Override
@@ -45,7 +65,14 @@ public class ConsistentHashing {
         }
         @Override
         public String toString(){
-           return this.virtualNodeName+":"+this.hashCode;
+           return this.virtualNodeName+":"+this.index;
+        }
+    }
+    static class Node{
+        public String name;
+        public List<Object> items = new ArrayList<>();
+        public Node(String name){
+            this.name = name;
         }
     }
 }
